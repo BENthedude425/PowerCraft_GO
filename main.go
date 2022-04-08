98b53d7bf17fe75e6ff3ddc8ba0244fe964d01fc
0x1a8510f2 <admin@0x1a8510f2.space>
2022-04-08 16:32:13 +0100

    Initial webserver

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main(){
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+)
 
+//go:embed all:ui
+var ui embed.FS
+
+func main() {
+	// Remove /ui/ prefix from ui embed FS
+	ui, _ := fs.Sub(ui, "ui")
+
+	// Create a HTTP multiplexer to handle requests to paths
+	mux := http.NewServeMux()
+
+	// Add handlers for paths
+
+	mux.Handle("/", http.FileServer(http.FS(ui)))
+
+	// Start the webserver
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
 }
